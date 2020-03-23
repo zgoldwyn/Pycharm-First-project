dd6b6e2506ab7bed3a7e5480112977aaeade9b22
zgoldwyn <zachgoldwyn@icloud.com>
2020-03-23 16:19:00 -0700

    created lists and more

--- PythonPractice.py
+++ PythonPractice.py
@@ -13,33 +13,55 @@ name = "Zach"
 x = 10
 #print(x)
 
-def LessThanGreaterThan(num):
+#def LessThanGreaterThan(num):
     #print(num)
 
-    if num > 10:
-        print("{} is greater than 10".format(num))
+  #  num = int(input("enter a number"))
+  #  print(num)
+  #  if num > 10:
+   #     print("{} is greater than 10".format(num))
 
-    if num < 10:
-        print("{} is less than 10".format(num))
-    if num == 10:
-        print("the number is 10")
-LessThanGreaterThan(10)
+    #if num < 10:
+       # print("{} is less than 10".format(num))
+    #if num == 10:
+     #   print("the number is 10")
+#LessThanGreaterThan(10)
 
 
 
 
 
-#3. Write a program that takes a number and tells the user if the
-#number is greater than 10, less than 10, or equal to 10. Don't
-#forget to convert the string into an integer.
+
+
+
+
+
+
+
 #4. Write a program that takes a number as a user input and tells
 #the user if it is even or odd.
 #5. Create a list and write a program that prints each individual
 #element to the console.
+
+favoriteFoods = ["donuts", "ice cream", "sushi", "pizza"]
+
+#print(favoriteFoods[2])
+for food in favoriteFoods:
+    print(food)
+
+
+
 #6. Write a program that uses a loop fill an empty list with 10
 #numbers and prints the list to the console. The numbers can
 #be random or simply put in the numbers 0-9 or 1-10 in
 #ascending order.
+
+myemptylist = []
+for number in range(11):
+    print(number)
+myemptylist.append(number)
+
+
 #7. Write a program that takes a number as user input and tells
 #you how many numbers in a list are less than the number that
 #the user gave.
