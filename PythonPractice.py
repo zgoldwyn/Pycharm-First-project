2271d02dbc451abf0472ad55c4ec1120bfaad915
zgoldwyn <zachgoldwyn@icloud.com>
2020-03-23 15:49:40 -0700

    created less than greater than

---
+++ PythonPractice.py
@@ -0,0 +1,49 @@
+def main():
+    print("Yay")
+
+if __name__ == "__main__":
+    main()
+
+name = "Zach"
+
+#print("hello {}".format(name))
+
+
+
+x = 10
+#print(x)
+
+def LessThanGreaterThan(num):
+    #print(num)
+
+    if num > 10:
+        print("{} is greater than 10".format(num))
+
+    if num < 10:
+        print("{} is less than 10".format(num))
+    if num == 10:
+        print("the number is 10")
+LessThanGreaterThan(10)
+
+
+
+
+
+#3. Write a program that takes a number and tells the user if the
+#number is greater than 10, less than 10, or equal to 10. Don't
+#forget to convert the string into an integer.
+#4. Write a program that takes a number as a user input and tells
+#the user if it is even or odd.
+#5. Create a list and write a program that prints each individual
+#element to the console.
+#6. Write a program that uses a loop fill an empty list with 10
+#numbers and prints the list to the console. The numbers can
+#be random or simply put in the numbers 0-9 or 1-10 in
+#ascending order.
+#7. Write a program that takes a number as user input and tells
+#you how many numbers in a list are less than the number that
+#the user gave.
+#8. Write a program that keeps asking the user to enter a word.
+#For each word that the user enters, the program prints out
+#each letter in the word on a new line. When the user enters
+#“s 
