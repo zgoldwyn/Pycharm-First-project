326079e7c9f78a09214153b66affbdbb17d34ea0
zgoldwyn <zachgoldwyn@icloud.com>
2020-03-24 10:38:54 -0700

    enter a number, volume and surface area calculator for rectangular prisms, cylinders, spheres, and cones(just volume)

--- PythonPractice.py
+++ PythonPractice.py
@@ -1,5 +1,5 @@
 def main():
-    print("Yay")
+    print(" ")
 
 if __name__ == "__main__":
     main()
@@ -9,40 +9,70 @@ name = "Zach"
 #print("hello {}".format(name))
 
 
-global x
-x = 50
-def change():
-    global x
-    x = 50
+#global x
+#x = 50
+#def change():
+ #   global x
+  #  x = 50
 #print(x)
 
-def LessThanGreaterThan(num):
-
-
-    num = int(input("enter a number "))
-    num = int(input("enter a number "))
-    if num > x:
-        print("{} is greater than 50".format(num))
-
-    if num < x:
-        print("{} is less than 50".format(num))
-    if num == x:
-        print("the number is 50")
-    if num == x:
-        print(" ")
-    else:
-            print("yay {} is your number".format(num))
-LessThanGreaterThan(x)
+#def LessThanGreaterThan(num):
 
 
 
+ #   num = int(input("enter a number "))
+  #  if num > x:
+      #  print("{} is greater than 50".format(num))
 
+    #if num < x:
+     #   print("{} is less than 50".format(num))
+    #if num == x:
+     #   print("the number is 50")
+    #if num == x:
+      #  print(" ")
+    #else:
+   #         print("yay {} is your number".format(num))
+#LessThanGreaterThan(x)
+pi = 3.1415
+print("Volume and Surface Area of Rectangular Prism")
 
+l = int(input("Length = "))
+w = int(input("Width = "))
+h = int(input("Height = "))
+vorp = l * w * h
+sorp = 2*l*w + 2*l*h + 2*w*h
+print("volume = {}un‎³".format(vorp))
+print("Surface Area = {}un²".format(sorp))
 
+print(" ")
 
+print("Volume and Surface Area of Cylinder")
 
+r = int(input("Radius = "))
+h = int(input("Height = "))
+voc = 3.1415 * r * r * h
+soc = (2 * pi * r * h)+(2 * pi * r * r)
+print("Volume = {}un‎³".format(voc))
+print("Surface Area = {}un²".format(soc))
 
+print(" ")
 
+print("Volume and Surface Area of Sphere")
+
+r = int(input("Radius = "))
+vos = 4.1887 * r * r * r
+sos = 4 * pi * r * r
+print("Volume = {}un‎³".format(vos))
+print("Surface Area = {}un²".format(sos))
+
+print(" ")
+
+print("Volume of Cone")
+
+r = int(input("Radius = "))
+h = int(input("Height = "))
+VolumeOfCone = pi * r * r * h / 3
+print("Volume = {}un‎³".format(VolumeOfCone))
 
 
 
